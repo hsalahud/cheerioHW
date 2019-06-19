98ddbfa7c289c7dbc19eb2427c849409dfc4982f
hamzasalah <hamzasalah@gmail.com>
2019-06-19 01:20:44 -0700

    adding process.env.PORT

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(express.json())
 require('./routes')(app)
 
 require('mongoose').connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
-  .then(_ => app.listen(3001))
+  .then(_ => app.listen(process.env.PORT || 3001))
   .catch(e => console.log(e))
